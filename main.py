1834da2c4ce08e43d332b7079f84633c8f1f9883
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-10-30 06:16:37 +0400

    testing webhook

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I'm still in development!")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(API_KEY).build()
