ae46b46820310bd605fbe12a11bf9388756afb39
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-10-30 05:16:55 +0400

    creating a folder to contain files the bot needs to run

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+load_dotenv()  # Loading .env file
+API_KEY = os.getenv('API_KEY')  # Extracting the API_KEY from .env file
+
+
+# To get INFO about the program while running
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(API_KEY).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
