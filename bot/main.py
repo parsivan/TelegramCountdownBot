3b3791660acff912f68025557986e1ea2fb116ce
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-11-01 21:35:00 +0400

    figuring out buttons | these are inline buttons

--- bot/main.py
+++ bot/main.py
@@ -2,9 +2,11 @@ import os
 import logging
 import asyncio
 import contextlib
+
 import datetime as dtm  # import timezone, date, datetime, time, timedelta
 from dotenv import load_dotenv
-from telegram import Update
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 
@@ -21,12 +23,29 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"How can I help you {update.message.chat.first_name}?")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi! How can I help you {update.message.chat.first_name}?")
+    keyboard = [
+        [
+            InlineKeyboardButton("Get a GF", callback_data="1"),
+            InlineKeyboardButton("Get a Life", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Quit Life", callback_data="3")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
-if __name__ == '__main__':
+async def echo_time():
+    ...
+
+
+def main() -> None:
     application = ApplicationBuilder().token(API_KEY).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     
     application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
