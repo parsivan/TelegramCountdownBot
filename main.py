11bc4a5907df54b6245b9045d3acfc83cdb56d8a
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-11-08 18:29:48 +0400

    moving all files to root of project | deleting requirements.txt to use uv manager

--- main.py
+++ main.py
@@ -11,26 +11,27 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 
 load_dotenv()  # Loading .env file
-API_KEY = os.getenv('API_KEY')  # Extracting the API_KEY from .env file
+API_KEY = os.getenv("API_KEY")  # Extracting the API_KEY from .env file
 
 
 # To get INFO about the program while running
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Respond to /start"""
     # await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi! How can I help you {update.message.chat.first_name}?")
-    await update.message.reply_text(f"Hi! How can I help you {update.message.chat.first_name}?")
+    await update.message.reply_text(
+        f"Hi! How can I help you {update.message.chat.first_name}?"
+    )
     keyboard = [
         [
             InlineKeyboardButton("Get a GF", callback_data="1"),
             InlineKeyboardButton("Get a Life", callback_data="2"),
         ],
-        [InlineKeyboardButton("Quit Life", callback_data="3")]
+        [InlineKeyboardButton("Quit Life", callback_data="3")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Please choose:", reply_markup=reply_markup)
@@ -44,16 +45,18 @@ async def echo_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Reply to /help and give documentation for using bot"""
-    await update.message.reply_text("/help you can set timezone using /zone and then get your local time using /time ")
+    await update.message.reply_text(
+        "/help you can set timezone using /zone and then get your local time using /time "
+    )
 
 
 def main() -> None:
     """Start the Bot."""
     application = ApplicationBuilder().token(API_KEY).build()
 
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('Help', help_handler))
-    application.add_handler(CommandHandler('time', echo_time))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("Help", help_handler))
+    application.add_handler(CommandHandler("time", echo_time))
 
     application.run_polling()
 
